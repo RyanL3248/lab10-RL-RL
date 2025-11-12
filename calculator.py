0b0913fc13b2e0be9391f333a61bde4e6a65acda
RyanB Lin <ryanl@RyanBs-MacBook-Air.local>
2025-11-12 10:41:57 -0600

    modified calc 2

--- calculator.py
+++ calculator.py
@@ -5,7 +5,37 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+
+import math 
+
+def square_root(a):
+    if a < 0:
+            raise ValueError("Cannot be less than 0")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)
+    except ValueError as e:
+        return str(e)
+
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    assert a != 0, "ZeroDivisionError"
+    return b / a 
+
+def log(a, b):
+    assert a != 0, "ValueError"
+    assert b != 0, "ValueError"
+    return math.log(b, a)
+
+def exp(a, b):
+    return math.pow(a, b)
