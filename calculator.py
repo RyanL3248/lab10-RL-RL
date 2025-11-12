d91ec9fd573d4908b991828b6ebe82b3dfede971
RyanB Lin <ryanl@RyanBs-MacBook-Air.local>
2025-11-12 10:50:26 -0600

    modified calc 5000

--- calculator.py
+++ calculator.py
@@ -1,5 +1,4 @@
-
-# GitHub Link: https://github.com/RyanL3248/lab10-RL-RL.git
+#https://github.com/RyanL3248/lab10-RL-RL.git
 #Partner 1: Ryan Linton
 #Partner 2: Ryan Linton
 
@@ -19,7 +18,7 @@ def hypotenuse(a, b):
 def add(a, b): 
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 def mul(a, b):
@@ -29,7 +28,7 @@ def div(a, b):
     assert a != 0, "ZeroDivisionError"
     return b / a 
 
-def log(a, b):
+def logarithm(a, b):
     assert a != 0, "ValueError"
     assert b != 0, "ValueError"
     return math.log(b, a)
