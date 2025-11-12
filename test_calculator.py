63dfd87fa7a817ed357e5d36561bd6b8205fef9c
RyanB Lin <ryanl@RyanBs-MacBook-Air.local>
2025-11-12 10:51:38 -0600

    im done test_calc

--- test_calculator.py
+++ test_calculator.py
@@ -13,9 +13,9 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, 3), 2)
 
     def test_subtract(self): # 3 assertions
-        self.assertEqual(sub(5, 2), 3)
-        self.assertEqual(sub(-4, -5), 1)
-        self.assertEqual(sub(-5, 2), -7)
+        self.assertEqual(subtract(5, 2), 3)
+        self.assertEqual(subtract(-4, -5), 1)
+        self.assertEqual(subtract(-5, 2), -7)
 
     ######## Partner 1
     def test_multiply(self): # 3 assertions
@@ -35,21 +35,21 @@ class TestCalculator(unittest.TestCase):
             div(0, 5)
 
     def test_logarithm(self): # 3 assertions
-        self.assertEqual(log(2, 8), 3)
-        self.assertEqual(log(4, 16), 2)
-        self.assertEqual(log(2, 2), 1)
+        self.assertEqual(logarithm(2, 8), 3)
+        self.assertEqual(logarithm(4, 16), 2)
+        self.assertEqual(logarithm(2, 2), 1)
 
 
     def test_log_invalid_base(self): # 1 assertion
     #     # use same technique from test_divide_by_zero
         with self.assertRaises(AssertionError):
-            log(5, 0)
+            logarithm(5, 0)
     
     ######## Partner 1
     def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
         with self.assertRaises(AssertionError):
-            log(0, 5)
+            logarithm(0, 5)
 
 
     def test_hypotenuse(self): # 3 assertions
